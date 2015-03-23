05361c6a930d3b65db271de798ff9c25a46e4189
devilgaia <devilgaia@hotmail.com>
2015-03-24 00:50:43 +0100

    arreglar part 1

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=edunetcat',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
