83597defa2a2b4dd502822882087a03885574ef6
devilgaia <devilgaia@hotmail.com>
2015-03-24 00:21:10 +0100

    separacio api - web

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=edunetcat',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
