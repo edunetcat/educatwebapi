ddc6b24db506c0f51b059c8ea8d81d76969046e3
devilgaia <devilgaia@hotmail.com>
2015-03-24 01:46:47 +0100

    format json peticions web en assignaturesController

--- controllers/AssignaturesController.php
+++ controllers/AssignaturesController.php
@@ -5,9 +5,16 @@ namespace app\controllers;
 use yii\rest\ActiveController;
 use yii\data\Pagination;
 use app\models\Assignatures;
+use yii\web\Response;
 
 class AssignaturesController extends ActiveController {
 	public $modelClass = 'app\models\Assignatures'; // no cal fer cap accio, nomes aixo i la resta ho fa el yii
+	public function behaviors() {
+		$behaviors = parent::behaviors ();
+		//configurem la resposta en format json cuan es demani desde web
+		$behaviors ['contentNegotiator'] ['formats'] ['text/html'] = Response::FORMAT_JSON;
+		return $behaviors;
+	}
 }
 
 ?>
