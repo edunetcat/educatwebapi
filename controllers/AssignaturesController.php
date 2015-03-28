48ea7994d291785dba717c4df483cf2adbb751d6
devilgaia <devilgaia@hotmail.com>
2015-03-28 14:21:16 +0100

    suport per a api key

--- controllers/AssignaturesController.php
+++ controllers/AssignaturesController.php
@@ -6,14 +6,28 @@ use yii\rest\ActiveController;
 use yii\data\Pagination;
 use app\models\Assignatures;
 use yii\web\Response;
+use yii\filters\AccessControl;
+use yii\filters\auth\HttpBasicAuth;
+use yii\helpers\ArrayHelper;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\auth\QueryParamAuth;
 
 class AssignaturesController extends ActiveController {
 	public $modelClass = 'app\models\Assignatures'; // no cal fer cap accio, nomes aixo i la resta ho fa el yii
 	public function behaviors() {
 		$behaviors = parent::behaviors ();
-		//configurem la resposta en format json cuan es demani desde web
+		// configurem la resposta en format json cuan es demani desde web
 		$behaviors ['contentNegotiator'] ['formats'] ['text/html'] = Response::FORMAT_JSON;
+		$behaviors ['authenticator'] = [ 
+				'class' => QueryParamAuth::className () 
+		]; // daquesta forma necesita la apikey
+
 		return $behaviors;
+	}
+	public function init() {
+		parent::init ();
+		\Yii::$app->user->enableSession = false;
 	}
 }
 
